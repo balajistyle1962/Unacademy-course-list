045c213516b9735bfd221518ead6edc62c5abe77
balajistyle1962 <77094465+balajistyle1962@users.noreply.github.com>
2023-03-19 17:53:00 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,22 @@
+function search() {
+    let filter = document.getElementById('search-container').value.toUpperCase();
+    let item = document.querySelectorAll('.slide');
+    let l = document.getElementsByTagName('h3');
+    for(var i = 0;i<=l.length;i++){
+    let a=item[i].getElementsByTagName('h3')[0];
+    let value=a.innerHTML || a.innerText || a.textContent;
+    if(value.toUpperCase().indexOf(filter) > -1) {
+    item[i].style.display="";
+    }
+    else
+    {
+    item[i].style.display="none";
+    }
+    }
+    }
+
+
+
+
+
+
